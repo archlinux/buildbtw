2a59d6e0142f7ddc99a691d6226c64bcb8060b72
Rafael Epplée <rafael@rafa.ee>
2024-09-10 17:18:34 +0200

    Finish preliminary version of build graph calculation

--- src/worker.rs
+++ src/worker.rs
@@ -3,6 +3,9 @@ use std::{collections::HashMap, fs::read_dir};
 
 use anyhow::{anyhow, Context, Result};
 use git2::Repository;
+use petgraph::graph::Edge;
+use petgraph::stable_graph::EdgeReference;
+use petgraph::visit::EdgeRef;
 use petgraph::{graph::NodeIndex, prelude::StableGraph, Graph};
 use srcinfo::Srcinfo;
 use tokio::{sync::mpsc::UnboundedSender, task::spawn_blocking};
@@ -110,11 +113,19 @@ async fn calculate_packages_to_be_built(
     // We have the global graph. Based on this, find the precise graph of dependents for the
     // given Pkgbases.
     let mut packages_to_be_built: BuildSetGraph = Graph::new();
-    let mut pkgbase_to_build_graph_node_index: HashMap<Pkgbase, NodeIndex> = HashMap::new();
+    let mut global_graph_to_build_graph_node_index: HashMap<NodeIndex, NodeIndex> = HashMap::new();
     let mut visited_global_graph_indexes = HashSet::new();
 
+    // from node, to node
+    // TODO we should just store edges here, but that makes initializing the
+    // algorithm more complicated
+    type NodeToVisit = (Option<NodeIndex>, NodeIndex);
+    // We'll update this while discovering new nodes that are reachable from our
+    // root nodes. To reconstruct edges in the new graph, we'll store the node we
+    // came from as well.
+    let mut nodes_to_visit: VecDeque<NodeToVisit> = VecDeque::new();
+
     // add root nodes from our build namespace so we can start walking the graph
-    let mut nodes_to_visit = VecDeque::new();
     for (pkgbase, branch) in &namespace.current_origin_changesets {
         let repo = Repository::open(format!("./source_repos/{pkgbase}"))?;
         let srcinfo = read_srcinfo_from_repo(&repo, branch)?;
@@ -123,39 +134,61 @@ async fn calculate_packages_to_be_built(
             let node_index = pkgname_to_node_index
                 .get(&pkgname)
                 .ok_or_else(|| anyhow!("Failed to get index for pkgname {pkgname}"))?;
-            // We're going to visit all dependents of this root node
-            nodes_to_visit.extend(global_graph.neighbors(*node_index));
+            nodes_to_visit.push_back((None, *node_index))
         }
     }
 
     // Walk through all transitive neighbors of our starting nodes to build a graph of nodes
     // that we want to rebuild
-    while let Some(package_index_to_visit) = nodes_to_visit.pop_front() {
-        // If we've visited this package already, skip it
-        if visited_global_graph_indexes.contains(&package_index_to_visit) {
-            continue;
+    while let Some((coming_from_node, global_node_index_to_visit)) = nodes_to_visit.pop_front() {
+        // Create build graph node if it doesn't exist
+        let build_graph_node_index = if let Some(index) =
+            global_graph_to_build_graph_node_index.get(&global_node_index_to_visit)
+        {
+            *index
+        } else {
+            // Find out the pkgbase of the package we're visiting
+            let package_node = global_graph
+                .node_weight(global_node_index_to_visit)
+                .ok_or_else(|| anyhow!("Failed to find node in global dependency graph"))?;
+            let (srcinfo, _) = pkgname_to_srcinfo_map
+                .get(&package_node.pkgname)
+                .ok_or_else(|| {
+                    anyhow!("Failed to get srcinfo for pkgname {}", package_node.pkgname)
+                })?;
+            let pkgbase = srcinfo.base.pkgbase.clone();
+
+            // Remember to visit this node's neighbors in the future
+            for edge in global_graph.edges(global_node_index_to_visit) {
+                nodes_to_visit.push_back((Some(edge.source()), edge.target()))
+            }
+
+            // Add this node to the buildset graph
+            let build_graph_node_index = packages_to_be_built.add_node(BuildPackageNode {
+                pkgbase: pkgbase.clone(),
+                commit_hash: package_node.commit_hash.clone(),
+            });
+            global_graph_to_build_graph_node_index
+                .insert(global_node_index_to_visit, build_graph_node_index);
+
+            // Remember that we now created a node for this package in the new graph
+            visited_global_graph_indexes.insert(global_node_index_to_visit);
+            build_graph_node_index
+        };
+
+        // If we stored the edge we used to get to this node,
+        // add it to the new graph we're building.
+        if let Some(coming_from_node) = coming_from_node {
+            packages_to_be_built.add_edge(
+                *global_graph_to_build_graph_node_index
+                    .get(&coming_from_node)
+                    .ok_or_else(|| {
+                        anyhow!("Failed to look up build set graph node when adding an edge")
+                    })?,
+                build_graph_node_index,
+                PackageBuildDependency {},
+            );
         }
-
-        // Find out the pkgbase of the package we're visiting
-        let package_node = global_graph
-            .node_weight(package_index_to_visit)
-            .ok_or_else(|| anyhow!("Failed to find node in global dependency graph"))?;
-        let (srcinfo, _) = pkgname_to_srcinfo_map
-            .get(&package_node.pkgname)
-            .ok_or_else(|| anyhow!("Failed to get srcinfo for pkgname {}", package_node.pkgname))?;
-        let pkgbase = srcinfo.pkg.pkgname.clone();
-
-        // Add this node and its edges to the buildset graph
-        let node_index = packages_to_be_built.add_node(BuildPackageNode {
-            pkgbase: pkgbase.clone(),
-            commit_hash: package_node.commit_hash.clone(),
-        });
-        pkgbase_to_build_graph_node_index.insert(pkgbase.clone(), node_index);
-        // TODO add edges!
-        // TODO add neighbors to visit
-
-        // Don't visit this node again
-        visited_global_graph_indexes.insert(package_index_to_visit);
     }
 
     Ok(packages_to_be_built)
