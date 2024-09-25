de5e0bfb1aecc17f89c47cb98f6d43a1c801b9af
Levente Polyak <anthraxx@archlinux.org>
2024-09-25 18:01:15 +0200

    client: move API calls to separate functions

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -1,7 +1,8 @@
 use crate::args::{Args, Command};
 use anyhow::{Context, Result};
-use buildbtw::{BuildNamespace, ScheduleBuildResult, SetBuildStatusResult};
+use buildbtw::{BuildNamespace, GitRepoRef, Pkgbase, ScheduleBuildResult, SetBuildStatusResult};
 use clap::Parser;
+use uuid::Uuid;
 
 mod args;
 
@@ -13,34 +14,10 @@ async fn main() -> Result<()> {
             name,
             origin_changesets,
         } => {
-            let create = buildbtw::CreateBuildNamespace {
-                name,
-                origin_changesets,
-            };
-
-            let response: BuildNamespace = reqwest::Client::new()
-                .post("http://0.0.0.0:8080/namespace")
-                .json(&create)
-                .send()
-                .await
-                .context("Failed to send to server")?
-                .json()
-                .await?;
-
-            println!("Created build namespace: {:?}", response);
+            create_namespace(name, origin_changesets).await?;
         }
         Command::ScheduleBuild { namespace } => {
-            println!("Building pending package for namespace: {:?}", namespace);
-
-            let response: ScheduleBuildResult = reqwest::Client::new()
-                .post(format!("http://0.0.0.0:8080/namespace/{namespace}/build"))
-                .send()
-                .await
-                .context("Failed to send to server")?
-                .json()
-                .await?;
-
-            println!("Scheduled build: {:?}", response);
+            schedule_build(namespace).await?;
         }
         Command::SetBuildStatus {
             namespace,
@@ -48,19 +25,71 @@ async fn main() -> Result<()> {
             pkgbase,
             status,
         } => {
-            let data = buildbtw::SetBuildStatus { status };
-
-            let response: SetBuildStatusResult = reqwest::Client::new()
-                .patch(format!("http://0.0.0.0:8080/namespace/{namespace}/iteration/{iteration}/pkgbase/{pkgbase}"))
-                .json(&data)
-                .send()
-                .await
-                .context("Failed to send to server")?
-                .json()
-                .await?;
-
-            println!("Set build status: {:?}", response);
+            set_build_status(namespace, iteration, pkgbase, status).await?;
+        }
+        Command::BuildNamespace { namespace } => {
+            build_namespace(namespace).await?;
         }
     }
     Ok(())
+}
+
+async fn create_namespace(
+    name: String,
+    origin_changesets: Vec<GitRepoRef>,
+) -> Result<BuildNamespace> {
+    let create = buildbtw::CreateBuildNamespace {
+        name,
+        origin_changesets,
+    };
+
+    let response: BuildNamespace = reqwest::Client::new()
+        .post("http://0.0.0.0:8080/namespace")
+        .json(&create)
+        .send()
+        .await
+        .context("Failed to send to server")?
+        .json()
+        .await?;
+
+    println!("Created build namespace: {:?}", response);
+    Ok(response)
+}
+
+async fn schedule_build(namespace: Uuid) -> Result<ScheduleBuildResult> {
+    println!("Building pending package for namespace: {:?}", namespace);
+
+    let response: ScheduleBuildResult = reqwest::Client::new()
+        .post(format!("http://0.0.0.0:8080/namespace/{namespace}/build"))
+        .send()
+        .await
+        .context("Failed to send to server")?
+        .json()
+        .await?;
+
+    println!("Scheduled build: {:?}", response);
+    Ok(response)
+}
+
+async fn set_build_status(
+    namespace: Uuid,
+    iteration: Uuid,
+    pkgbase: Pkgbase,
+    status: buildbtw::PackageBuildStatus,
+) -> Result<SetBuildStatusResult> {
+    let data = buildbtw::SetBuildStatus { status };
+
+    let response: SetBuildStatusResult = reqwest::Client::new()
+        .patch(format!(
+            "http://0.0.0.0:8080/namespace/{namespace}/iteration/{iteration}/pkgbase/{pkgbase}"
+        ))
+        .json(&data)
+        .send()
+        .await
+        .context("Failed to send to server")?
+        .json()
+        .await?;
+
+    println!("Set build status: {:?}", response);
+    Ok(response)
 }
