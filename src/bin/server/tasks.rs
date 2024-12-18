b856a2f0b69e685550f9954e9dbb099b7114fb52
Rafael Epplée <rafael@rafa.ee>
2024-12-18 10:48:29 +0100

    Dispatch builds in a background loop

--- src/bin/server/tasks.rs
+++ src/bin/server/tasks.rs
@@ -9,7 +9,6 @@ use gitlab::{AsyncGitlab, GitlabBuilder};
 use redact::Secret;
 use sqlx::SqlitePool;
 use tokio::sync::mpsc::UnboundedSender;
-use tokio::time::sleep;
 use uuid::Uuid;
 
 use crate::{
@@ -41,7 +40,7 @@ pub async fn start(
     let periodic_check_pool = pool.clone();
     tokio::spawn(async move {
         loop {
-            match maybe_create_new_iterations_for_all_namespaces(&periodic_check_pool).await {
+            match update_and_build_all_namespaces(&periodic_check_pool).await {
                 Ok(_) => {}
                 Err(e) => println!("Error creating new iteration: {e:?}"),
             };
@@ -53,17 +52,18 @@ pub async fn start(
         let gitlab_client = GitlabBuilder::new("gitlab.archlinux.org", token.expose_secret())
             .build_async()
             .await?;
-        fetch_source_repo_changes_in_loop(gitlab_client, pool);
+        fetch_source_repo_changes_in_loop(gitlab_client, pool.clone());
     }
 
     Ok(sender)
 }
 
-async fn maybe_create_new_iterations_for_all_namespaces(pool: &SqlitePool) -> Result<()> {
+async fn update_and_build_all_namespaces(pool: &SqlitePool) -> Result<()> {
     // Check all build namespaces and see if they need a new iteration.
     let namespaces = db::namespace::list(pool).await?;
     for namespace in namespaces {
-        create_new_namespace_iteration_if_needed(pool, namespace).await?;
+        create_new_namespace_iteration_if_needed(pool, &namespace).await?;
+        schedule_next_build_if_needed(pool, &namespace).await?;
     }
 
     Ok(())
@@ -91,36 +91,19 @@ pub fn fetch_source_repo_changes_in_loop(client: AsyncGitlab, db_pool: SqlitePoo
     });
 }
 
-async fn build_new_namespace(id: Uuid, pool: &SqlitePool) -> Result<()> {
-    let namespace = db::namespace::read(id, pool).await?;
-
-    println!("Adding namespace: {namespace:#?}");
-
-    let future_pool = pool.clone();
-    let future = || async {
-        let result = build_namespace(future_pool, namespace).await;
-        if let Err(e) = result {
-            println!("{e:?}");
-        }
-    };
-    tokio::spawn(future());
-
-    Ok(())
-}
-
 async fn create_new_namespace_iteration_if_needed(
     pool: &SqlitePool,
-    namespace: BuildNamespace,
+    namespace: &BuildNamespace,
 ) -> Result<()> {
     let previous_iterations = db::iteration::list(pool, namespace.id).await?;
-    let new_iteration = new_build_set_iteration_is_needed(&namespace, &previous_iterations).await?;
+    let new_iteration = new_build_set_iteration_is_needed(namespace, &previous_iterations).await?;
 
     match new_iteration {
         NewBuildIterationResult::NewIterationNeeded {
             packages_to_build,
             reason,
         } => {
-            let namespace_name = namespace.name;
+            let namespace_name = namespace.name.clone();
             println!(
                 "Creating new build iteration for namespace {namespace_name}, reason: {reason:?}"
             );
@@ -133,11 +116,6 @@ async fn create_new_namespace_iteration_if_needed(
             };
 
             db::iteration::create(pool, namespace.id, new_iteration).await?;
-
-            match build_new_namespace(namespace.id, pool).await {
-                Ok(_) => {}
-                Err(e) => println!("Error building newest iteration of namespace: {e:?}"),
-            }
         }
         NewBuildIterationResult::NoNewIterationNeeded => {}
     }
@@ -146,34 +124,30 @@ async fn create_new_namespace_iteration_if_needed(
 }
 
 // TODO this needs to be dispatched in a background loop as well
-async fn build_namespace(pool: SqlitePool, namespace: BuildNamespace) -> Result<()> {
-    // while namespace is not fully built or blocked
-    loop {
-        // -> schedule build
-        let iteration = db::iteration::read_newest(&pool, namespace.id).await?;
-        let build = schedule_next_build_in_graph(&iteration, namespace.id).await;
-        match build {
-            // TODO: distinguish between no pending packages and failed graph
-            ScheduleBuildResult::NoPendingPackages => {
-                println!("No pending packages, retry in 5 seconds");
-                sleep(std::time::Duration::from_secs(5)).await;
-            }
-            ScheduleBuildResult::Scheduled(response) => {
-                schedule_build(&response).await?;
-                db::iteration::update(
-                    &pool,
-                    db::iteration::BuildSetIterationUpdate {
-                        id: iteration.id,
-                        packages_to_be_built: response.updated_build_set_graph.clone(),
-                    },
-                )
-                .await?;
-                println!("Scheduled build: {:?}", response.source);
-            }
-            ScheduleBuildResult::Finished => {
-                println!("Graph finished");
-                break;
-            }
+async fn schedule_next_build_if_needed(
+    pool: &SqlitePool,
+    namespace: &BuildNamespace,
+) -> Result<()> {
+    // -> schedule build
+    let iteration = db::iteration::read_newest(pool, namespace.id).await?;
+    let build = schedule_next_build_in_graph(&iteration, namespace.id).await;
+    match build {
+        // TODO: distinguish between no pending packages and failed graph
+        ScheduleBuildResult::NoPendingPackages => {}
+        ScheduleBuildResult::Scheduled(response) => {
+            schedule_build(&response).await?;
+            db::iteration::update(
+                pool,
+                db::iteration::BuildSetIterationUpdate {
+                    id: iteration.id,
+                    packages_to_be_built: response.updated_build_set_graph.clone(),
+                },
+            )
+            .await?;
+            println!("Scheduled build: {:?}", response.source);
+        }
+        ScheduleBuildResult::Finished => {
+            println!("Graph finished");
         }
     }
 
