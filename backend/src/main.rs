9f86f381757dfba8e74b8891ab650391065d7c93
Sven-Hendrik Haase <svenstaro@gmail.com>
2025-08-07 16:45:05 +0200

    Add tokio and color-eyre

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use color_eyre::Result;
+
+use crate::args::Args;
+mod args;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::parse();
+
+    Ok(())
 }
