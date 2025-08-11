d9d499e175cba335cbc233174a427439443649eb
Rafael Epplée <hello@rafa.ee>
2025-08-11 14:51:25 +0200

    Rename source files for consistency
    According to the cargo docs, source files for binaries are in
    kebab-kase, while source files in modules are snake_case.

--- src/pacman_repository.rs
+++ src/pacman_repository.rs
@@ -7,4 +7,3 @@
 //! Repositories are stored at
 //! `./data/repos/{namespace}_{iteration}/os/{architecture}/` and served
 //! for dependency resolution during subsequent builds in the same iteration.
-fn main() {}
