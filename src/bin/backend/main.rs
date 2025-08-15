d1c2542741dac2b2be6fb93e13b64de2acb562a5
Rafael Epplée <hello@rafa.ee>
2025-08-15 15:51:58 +0200

    Add axum web server

--- src/bin/backend/main.rs
+++ src/bin/backend/main.rs
@@ -7,13 +7,17 @@
 //! It coordinates with the local worker or GitLab runners to process package
 //! builds in VMs.
 
+use std::net::IpAddr;
+
 use clap::Parser;
-use color_eyre::Result;
+use color_eyre::{Result, eyre::Context};
+use tokio::{net::TcpListener, signal};
 
 use crate::args::Args;
 
 mod args;
 mod migrations;
+mod router;
 mod schema;
 #[cfg(test)]
 mod tests;
@@ -23,8 +27,9 @@ async fn main() -> Result<()> {
     let args = Args::parse();
 
     match args.command {
-        args::Command::Run {} => {
+        args::Command::Run { interface, port } => {
             let _db = schema::create_migrate_connect(&args.database_file).await?;
+            run_server(interface, port).await?;
         }
         args::Command::MigrateDatabase {} => {
             schema::create_migrate_connect(&args.database_file).await?;
@@ -32,4 +37,44 @@ async fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+async fn run_server(interface: IpAddr, port: u16) -> Result<()> {
+    let router = router::new();
+    let listener = TcpListener::bind(format!("{interface}:{port}")).await?;
+
+    axum::serve(listener, router)
+        .with_graceful_shutdown(shutdown_signal())
+        .await?;
+
+    Ok(())
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .wrap_err("failed to install Ctrl+C handler")?;
+        Ok::<(), color_eyre::Report>(())
+    };
+
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .wrap_err("failed to install signal handler")?
+            .recv()
+            .await;
+
+        Ok::<(), color_eyre::Report>(())
+    };
+
+    tokio::select! {
+        _ = ctrl_c => {
+            // todo: replace this once we have tracing
+            println!("Received SIGINT, shutting down...")
+        },
+        _ = terminate => {
+            // todo: replace this once we have tracing
+            println!("Received SIGTERM, shutting down...")
+        },
+    }
 }
---
+++ src/bin/backend/router.rs
@@ -0,0 +1,12 @@
+use std::time::Duration;
+
+use axum::Router;
+use tower_http::timeout::TimeoutLayer;
+
+pub fn new() -> Router {
+    Router::new().layer((
+        // Graceful shutdown will wait for outstanding requests to complete. Add a timeout so
+        // requests don't hang forever.
+        TimeoutLayer::new(Duration::from_secs(10)),
+    ))
+}
