3f22b0997520b4b71babdeb32d99120eaca0cc03
Rafael Epplée <hello@rafa.ee>
2025-08-22 11:27:43 +0200

    Format db_fields.rs

--- src/bin/backend/db_fields.rs
+++ src/bin/backend/db_fields.rs
@@ -26,14 +26,19 @@ use strum::{Display, EnumString};
 pub enum BuildStatus {
     /// Other failed builds are blocking this build from running
     Blocked,
+
     /// This is waiting to be scheduled
     Pending,
+
     /// Sent to the worker to build
     Scheduled,
+
     /// Worker has started building
     Building,
+
     /// Build has succeeded
     Built,
+
     /// Build as failed
     Failed,
 }
