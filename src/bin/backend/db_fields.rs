53d706fff086ba1c144da065e3d8fb61f1f6774c
Rafael Epplée <hello@rafa.ee>
2026-01-27 18:18:43 +0100

    Implement Display for TextUuid

--- src/bin/backend/db_fields.rs
+++ src/bin/backend/db_fields.rs
@@ -253,6 +253,12 @@ impl From<TextUuid> for sea_query::Value {
     }
 }
 
+impl std::fmt::Display for TextUuid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
 impl TryGetable for TextUuid {
     fn try_get_by<I: sea_orm::ColIdx>(
         res: &sea_orm::QueryResult,
