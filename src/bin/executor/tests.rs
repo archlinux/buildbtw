a81940efd6578d0206ea3ec6dbdf6855ce289202
Levente Polyak <anthraxx@archlinux.org>
2026-03-17 20:21:58 +0100

    executor: add test case for remote cloned package repository build

--- src/bin/executor/tests.rs
+++ src/bin/executor/tests.rs
@@ -1,5 +1,12 @@
 use alpm_types::{PKGBUILD_FILE_NAME, SRCINFO_FILE_NAME};
-use color_eyre::Result;
+use color_eyre::{
+    Result,
+    eyre::{bail, eyre},
+};
+
+use rstest::*;
+use std::{process::Stdio, time::Duration};
+use tokio::process::Command;
 
 use crate::run::build_project_dir;
 
@@ -81,6 +88,44 @@ arch=(any)
             .is_err(),
         "Build must fail on broken pkgbuild"
     );
+
+    Ok(())
+}
+
+#[tokio::test]
+#[ignore = "Test depends on an external resource and is heavyweight."]
+#[rstest]
+#[timeout(Duration::from_secs(120))]
+async fn test_gitlab_executor_build_project_dir_from_pkgctl_repo_clone() -> Result<()> {
+    let test_project_dir = tempfile::Builder::new()
+        .prefix("buildbtw-test-dir-")
+        .tempdir()?;
+    let test_output_dir = tempfile::Builder::new()
+        .prefix("buildbtw-test-dir-")
+        .tempdir()?;
+
+    let mut cmd = Command::new("pkgctl");
+    cmd.args(["repo", "clone", "--protocol", "https", "git-smash"])
+        .current_dir(test_project_dir.path())
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit());
+
+    let child = cmd
+        .spawn()
+        .map_err(|e| eyre!("Failed to spawn command '{:?}': {}", cmd.as_std(), e))?;
+    let output = child.wait_with_output().await?;
+    if !output.status.success() {
+        bail!("Failed to clone remote package repository");
+    }
+
+    build_project_dir(
+        test_project_dir.path().join("git-smash").as_path(),
+        test_output_dir.path(),
+        None,
+        120,
+    )
+    .await?;
 
     Ok(())
 }
