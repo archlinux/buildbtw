679cf2f4949ab1bedf6ea5c5e5ab3be88d055c19
Sven-Hendrik Haase <svenstaro@gmail.com>
2026-04-28 15:30:13 +0200

    Install color_eyre without colors if colors aren't supported
    This should clean up our production logs a lot because those have a ton of color control characters in them.

--- src/error_handler.rs
+++ src/error_handler.rs
@@ -13,10 +13,20 @@ pub fn init(verbose: u8) -> Result<()> {
         }
     }
 
+    let mut eyre_builder = color_eyre::config::HookBuilder::new();
+
+    // If colors aren't supported, we'll use the empty theme which effectively disables color output.
+    // See also:
+    // - https://github.com/eyre-rs/eyre/issues/236
+    // - https://github.com/eyre-rs/eyre/issues/237
+    // - https://github.com/eyre-rs/eyre/pull/238
+    if !colored::control::ShouldColorize::from_env().should_colorize() {
+        eyre_builder = eyre_builder.theme(color_eyre::config::Theme::new());
+    }
+
     // Custom color_eyre config so that we hide the env nagging section to get cleaner output in the
     // default case.
-    color_eyre::config::HookBuilder::new()
-        .panic_section("If you believe this is a program error, consider reporting a bug at https://gitlab.archlinux.org/archlinux/buildbtw")
+    eyre_builder.panic_section("If you believe this is a program error, consider reporting a bug at https://gitlab.archlinux.org/archlinux/buildbtw")
         .display_env_section(verbose != 0)
         .install()?;
 
