e7c858ab21bd5be2d061c1c74aa3dda5a6b68a1f
Rafael Epplée <hello@rafa.ee>
2026-05-26 17:05:54 +0200

    bbtw: add flag for overriding xdg state home

--- src/bin/bbtw/auth.rs
+++ src/bin/bbtw/auth.rs
@@ -1,6 +1,7 @@
 use std::io::{self, Write};
 
 use buildbtw::bbtw;
+use camino::Utf8PathBuf;
 use color_eyre::Result;
 use colored::Colorize;
 use time::OffsetDateTime;
@@ -12,8 +13,8 @@ use crate::api;
 use crate::args::AuthCommand;
 
 #[instrument]
-pub async fn login(server_url: Url) -> Result<()> {
-    if let Some(auth_token) = bbtw::auth::Token::read().await? {
+pub async fn login(server_url: Url, override_state_dir: Option<Utf8PathBuf>) -> Result<()> {
+    if let Some(auth_token) = bbtw::auth::Token::read(override_state_dir.clone()).await? {
         eprintln!(
             "{} You're already logged in as (since {}).",
             "Warning:".yellow().bold(),
@@ -35,7 +36,7 @@ pub async fn login(server_url: Url) -> Result<()> {
     println!("\nYou'll need to log in first via OIDC if you haven't already.");
     println!("Then click 'Create CLI Session' and copy the generated token.\n");
 
-    let client = api::Client::new(server_url).await?;
+    let client = api::Client::new(server_url, override_state_dir.clone()).await?;
 
     let auth_token = loop {
         print!("{}", "Paste your CLI session token here: ".bold());
@@ -68,7 +69,9 @@ pub async fn login(server_url: Url) -> Result<()> {
         );
     };
 
-    auth_token.persist(&bbtw::auth::token_path()?).await?;
+    auth_token
+        .persist(&bbtw::auth::token_path(override_state_dir)?)
+        .await?;
 
     println!("\n{}", "Successfully logged in!".bright_green().bold());
 
@@ -76,10 +79,11 @@ pub async fn login(server_url: Url) -> Result<()> {
 }
 
 #[instrument]
-pub async fn status(server_url: Url) -> Result<()> {
-    if let Some(auth_token) = bbtw::auth::Token::read().await? {
+pub async fn status(server_url: Url, override_state_dir: Option<Utf8PathBuf>) -> Result<()> {
+    if let Some(auth_token) = bbtw::auth::Token::read(override_state_dir.clone()).await? {
         // We'll verify that we're actually logged in properly and that the session is valid.
-        let user = api::user::current(&api::Client::new(server_url).await?).await?;
+        let user =
+            api::user::current(&api::Client::new(server_url, override_state_dir).await?).await?;
         if let Some(user) = user {
             println!(
                 "Logged in as {} (since {})",
@@ -101,9 +105,13 @@ pub async fn status(server_url: Url) -> Result<()> {
     Ok(())
 }
 
-pub async fn auth(auth_command: AuthCommand, server_url: Url) -> Result<()> {
+pub async fn auth(
+    auth_command: &AuthCommand,
+    server_url: Url,
+    override_state_dir: Option<Utf8PathBuf>,
+) -> Result<()> {
     match auth_command {
-        AuthCommand::Login => login(server_url).await,
-        AuthCommand::Status => status(server_url).await,
+        AuthCommand::Login => login(server_url, override_state_dir).await,
+        AuthCommand::Status => status(server_url, override_state_dir).await,
     }
 }
