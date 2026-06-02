5e97c3bc4fb9c9be617a77da9fc739a7ca0618c6
Rafael Epplée <hello@rafa.ee>
2026-06-02 15:42:39 +0200

    Add configuration for where builds should be dispatched

---
+++ src/schedule_builds.rs
@@ -0,0 +1,39 @@
+use color_eyre::{
+    Result,
+    eyre::{OptionExt, eyre},
+};
+use derive_more::Display;
+use sea_orm::DatabaseTransaction;
+
+use crate::{gitlab_api, queries};
+
+#[derive(Debug)]
+pub enum Config {
+    Gitlab(gitlab_api::Config),
+    Local,
+}
+
+#[derive(Display, Debug, Clone, PartialEq, Eq, Copy)]
+pub enum DispatchBuildsTo {
+    /// Create a gitlab pipeline for each build.
+    GitlabPipelines,
+    /// Run builds by spawning vmexec processes from the server.
+    LocalExecutor,
+}
+
+impl Config {
+    pub fn new(
+        dispatch_builds_to: Option<DispatchBuildsTo>,
+        maybe_gitlab: Option<gitlab_api::Config>,
+    ) -> Result<Option<Config>> {
+        Ok(match dispatch_builds_to {
+            Some(DispatchBuildsTo::GitlabPipelines) => {
+                Some(Config::Gitlab(maybe_gitlab.ok_or_eyre(
+                    "Gitlab config must be set for dispatching builds to gitlab pipelines",
+                )?))
+            }
+            Some(DispatchBuildsTo::LocalExecutor) => Some(Config::Local),
+            None => None,
+        })
+    }
+}
