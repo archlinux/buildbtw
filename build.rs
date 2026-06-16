e14cef9e82783a2171555902a19cff14c030a283
Rafael Epplée <hello@rafa.ee>
2026-06-16 13:06:43 +0200

    Fix graphql schema path in build.rs

--- build.rs
+++ build.rs
@@ -2,5 +2,5 @@
 
 fn main() {
     // Re-check GraphQL queries if the GraphQL schema changes.
-    println!("cargo::rerun-if-changed=src/gitlab/graphql_schema.json");
+    println!("cargo::rerun-if-changed=src/gitlab_api/graphql_schema.json");
 }
