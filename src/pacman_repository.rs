3bfc65efd3c252e74d6734328180b587e440ddf8
Levente Polyak <anthraxx@archlinux.org>
2026-06-23 23:16:39 +0200

    upload: Add build artifact to pacman database repo

--- src/pacman_repository.rs
+++ src/pacman_repository.rs
@@ -6,3 +6,68 @@
 //!
 //! Repositories are stored in buildbtw's configured artifact directory and served
 //! for dependency resolution during subsequent builds in the same iteration.
+
+use std::process::Stdio;
+
+use camino::Utf8PathBuf;
+use color_eyre::{
+    Result,
+    eyre::{bail, eyre},
+};
+use tokio::process::Command;
+use tracing::debug;
+
+use crate::{builds::build_repo_path, entities};
+
+/// Returns the filename for a pacman package database of a buildspace.
+#[must_use]
+pub fn pacman_repo_database_filename(buildspace: &entities::buildspaces::ModelEx) -> String {
+    format!("{}.db.tar.zst", buildspace.name)
+}
+
+/// Returns the pacman package database path within the artifacts data storage
+/// that belongs to a build of an iteration and buildspace.
+pub fn pacman_repo_database_path(
+    buildspace: &entities::buildspaces::ModelEx,
+    iteration: &entities::iterations::ModelEx,
+    build: &entities::builds::ModelEx,
+    override_data_dir: &Option<Utf8PathBuf>,
+) -> Result<Utf8PathBuf> {
+    let dest_dir = build_repo_path(buildspace, iteration, build, override_data_dir)?;
+    let db = dest_dir.join(pacman_repo_database_filename(buildspace));
+    Ok(db)
+}
+
+/// Add a package artifact to the pacman repository of a build iteration.
+pub async fn pacman_repo_add(
+    buildspace: &entities::buildspaces::ModelEx,
+    iteration: &entities::iterations::ModelEx,
+    build: &entities::builds::ModelEx,
+    packages: &[Utf8PathBuf],
+    override_data_dir: &Option<Utf8PathBuf>,
+) -> Result<()> {
+    let db = pacman_repo_database_path(buildspace, iteration, build, override_data_dir)?;
+
+    let mut cmd = Command::new("repo-add");
+    cmd.args(["--prevent-downgrade", "--wait-for-lock", db.as_ref()]);
+    cmd.args(packages);
+
+    cmd.stdin(Stdio::null())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped());
+
+    let child = cmd.spawn().map_err(|e| {
+        eyre!(
+            "Failed to spawn repo-add command '{:?}': {}",
+            cmd.as_std(),
+            e
+        )
+    })?;
+    let output = child.wait_with_output().await?;
+    if !output.status.success() {
+        bail!("Failed to run repo-add for {packages:?}: {:?}", output);
+    }
+
+    debug!("Successfully run repo-add for {packages:?}: {:?}", output);
+    Ok(())
+}
