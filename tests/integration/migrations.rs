f1677366f8c99c7ca2722b9dfeb44adbb06eb68d
Rafael Epplée <hello@rafa.ee>
2026-06-30 13:49:19 +0200

    Don't log migration queries in tests
    This added a lot of noise when reading logs of failing tests.

--- tests/integration/migrations.rs
+++ tests/integration/migrations.rs
@@ -2,12 +2,14 @@ use color_eyre::Result;
 use sea_orm::Database;
 use sea_orm_migration::MigratorTrait;
 
-use buildbtw::migrations::Migrator;
+use buildbtw::{migrations::Migrator, tracing};
 
 #[tokio::test]
 /// Check that the migrations don't raise an error when run on an empty
 /// database.
 async fn can_migrate() -> Result<()> {
+    tracing::init(0, false)?;
+
     let db = Database::connect("sqlite::memory:").await?;
 
     Migrator::up(&db, None).await?;
