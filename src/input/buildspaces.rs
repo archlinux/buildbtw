21b401c8613403675164cb9ac6cc8a531eb8b551
Rafael Epplée <hello@rafa.ee>
2026-07-01 13:07:50 +0200

    Implement bbtw new

--- src/input/buildspaces.rs
+++ src/input/buildspaces.rs
@@ -1,32 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use crate::{buildspace::BuildspaceSlug, git, input::garde_report, package};
-
-/// A changeset in a create buildspace request.
-/// It's a separate struct so we can add an opinionated default
-/// value for the branch name.
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CreateChangeset {
-    pub repo_slug: package::RepositorySlug,
-
-    // Can't use serde's default attr here because it doesn't work
-    // with `null` values: https://github.com/serde-rs/serde/issues/1098
-    pub branch_name: Option<git::BranchName>,
-}
-
-impl From<CreateChangeset> for git::Changeset {
-    fn from(
-        CreateChangeset {
-            repo_slug,
-            branch_name,
-        }: CreateChangeset,
-    ) -> Self {
-        git::Changeset {
-            repo_slug,
-            branch_name: branch_name.unwrap_or_else(git::BranchName::main),
-        }
-    }
-}
+use crate::{buildspace::BuildspaceSlug, git, input::garde_report};
 
 /// Input for creating a new buildspace.
 #[derive(Debug, Serialize, Deserialize)]
@@ -34,7 +8,7 @@ pub struct Create {
     /// If left out, will use the repo name of the first changeset.
     pub name: Option<BuildspaceSlug>,
 
-    pub changesets: Vec<CreateChangeset>,
+    pub changesets: git::Changesets,
 }
 
 /// Input, validated and transformed to use our newtypes.
@@ -49,16 +23,8 @@ impl TryFrom<Create> for ValidatedCreate {
 
     /// Validate and transform the input to our newtypes, returning a garde Report if anything fails.
     fn try_from(value: Create) -> Result<Self, Self::Error> {
-        // Convert changesets to our newtype
-        let changesets: git::Changesets = value
-            .changesets
-            .into_iter()
-            .map(git::Changeset::from)
-            .collect::<Vec<_>>()
-            .into();
-
         // Validate that changesets are not empty (a buildspace without changesets would build nothing)
-        let first_changeset = changesets.0.first().ok_or_else(|| {
+        let first_changeset = value.changesets.0.first().ok_or_else(|| {
             garde_report(
                 garde::Path::new("changesets"),
                 garde::Error::new("must not be empty"),
@@ -78,6 +44,9 @@ impl TryFrom<Create> for ValidatedCreate {
                     })?,
             );
 
-        Ok(ValidatedCreate { name, changesets })
+        Ok(ValidatedCreate {
+            name,
+            changesets: value.changesets,
+        })
     }
 }
